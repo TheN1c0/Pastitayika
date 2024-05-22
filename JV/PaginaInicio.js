f75c652fe97d20865774a2874cfe092b3dd5163e
Nicolas Bravo <nicojjj4@gmail.com>
2024-05-22 04:11:33 -0400

    Se añadieron efectos a la descripcion, ajustes de dimensiones, color de sec izquierda y una descripcion por defecto

--- JV/PaginaInicio.js
+++ JV/PaginaInicio.js
@@ -1,3 +1,6 @@
+
+
+
 function mostrarContenido(id) {
     // Oculta todos los contenidos
     
@@ -7,23 +10,32 @@ function mostrarContenido(id) {
     document.querySelectorAll('[id^="contenido"]').forEach(element => {
         element.style.display = 'none';/* esto los oculta */
     });
-
+    
     // Muestra el contenido especificado por su ID
     document.getElementById(id).style.display = 'block';
 }
 
 function mostrarDescri(id) {
     // Oculta todos los contenidos
-    
-    /* queryselectorall selecciona varios ids, ^ hace que seleccione todos los ides que comiencen con "contenido" */
-   /*  La función itera sobre todos los elementos seleccionados usando forEach 
-    y establece el estilo display de cada elemento en none, lo que los oculta visualmente en la página. */
     document.querySelectorAll('[id^="Descripcion"]').forEach(element => {
-        element.style.display = 'none';/* esto los oculta */
+        element.style.display = 'none';  // Oculta los elementos
+        element.style.opacity = 0;       // Resetea la opacidad
     });
 
-    // Muestra el contenido especificado por su ID
-    
-    document.getElementById(id).style.display = 'block';
+    // Obtiene el elemento a mostrar
+    var elemento = document.getElementById(id);
+    elemento.style.display = 'block';  // Hace visible el elemento
+    elemento.style.opacity = 0;        // Establece la opacidad inicial
+
+    // Incrementa la opacidad gradualmente
+    var opacidad = 0;
+    var interval = setInterval(function() {
+        if (opacidad < 1) {
+            opacidad += 0.1;  // Incrementa la opacidad
+            elemento.style.opacity = opacidad;
+        } else {
+            clearInterval(interval);  // Detiene la animación cuando la opacidad es 1
+        }
+    }, 100); 
     
 }
