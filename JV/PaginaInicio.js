ed8ec3711d4d8a6509c81ca527b04d082a3447b3
Nicolas Bravo <nicojjj4@gmail.com>
2024-05-22 22:14:59 -0400

    Se cambio la descripción de la seccion derecha, por una pestaña

--- JV/PaginaInicio.js
+++ JV/PaginaInicio.js
@@ -1,40 +1,38 @@
 
 
 
-function mostrarContenido(id) {
+function ocultarPestana() {
     // Oculta todos los contenidos
     
     /* queryselectorall selecciona varios ids, ^ hace que seleccione todos los ides que comiencen con "contenido" */
    /*  La función itera sobre todos los elementos seleccionados usando forEach 
     y establece el estilo display de cada elemento en none, lo que los oculta visualmente en la página. */
-    document.querySelectorAll('[id^="contenido"]').forEach(element => {
+    document.querySelectorAll('[id^="pestana"]').forEach(element => {
         element.style.display = 'none';/* esto los oculta */
     });
     
-    // Muestra el contenido especificado por su ID
-    document.getElementById(id).style.display = 'block';
 }
 
 function mostrarDescri(id) {
     // Oculta todos los contenidos
-    document.querySelectorAll('[id^="Descripcion"]').forEach(element => {
-        element.style.display = 'none';  // Oculta los elementos
-        element.style.opacity = 0;       // Resetea la opacidad
+    document.querySelectorAll('[id^="pestana"]').forEach(element => {
+        element.style.display = 'none';  
+        element.style.opacity = 0;       
     });
 
     // Obtiene el elemento a mostrar
     var elemento = document.getElementById(id);
-    elemento.style.display = 'block';  // Hace visible el elemento
-    elemento.style.opacity = 0;        // Establece la opacidad inicial
+    elemento.style.display = 'block';  
+    elemento.style.opacity = 0;        
 
-    // Incrementa la opacidad gradualmente
+    
     var opacidad = 0;
     var interval = setInterval(function() {
         if (opacidad < 1) {
-            opacidad += 0.1;  // Incrementa la opacidad
+            opacidad += 0.1;  
             elemento.style.opacity = opacidad;
         } else {
-            clearInterval(interval);  // Detiene la animación cuando la opacidad es 1
+            clearInterval(interval);  
         }
     }, 100); 
     
